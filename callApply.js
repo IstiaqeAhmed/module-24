fae0d479f99f216abdf954d6f1afc907d583961d
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-06 21:53:45 +0600

    25-4 difference between bind, call and apply

--- callApply.js
+++ callApply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this);
-       this.salary = this.salary - amount;
+       this.salary = this.salary - amount - tips - tax;
        return this.salary;
     }
 }
@@ -23,9 +23,26 @@ const friendlyPerson = {
     salary:25000
 }
 //normalPerson.chargeBill();
- const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
- heroChargeBill(2000);
- heroChargeBill(3000);
- console.log(heroPerson.salary);
- console.log(normalPerson.salary);
+//  const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+//  heroChargeBill(2000);
+//  heroChargeBill(3000);
+//  console.log(heroPerson.salary);
+//  console.log(normalPerson.salary);
 // Bind Method: return hishebe arekta function dibe
+
+
+
+// onno ekta object er modde call method call kora
+
+// normalPerson.chargeBill.call(heroPerson, 900, 100, 10);
+// normalPerson.chargeBill.call(heroPerson, 3000, 300, 30)
+// console.log(heroPerson.salary);
+
+// normalPerson.chargeBill.call(friendlyPerson, 5000, 500, 50);
+// console.log(friendlyPerson.salary);
+// console.log(normalPerson.salary);
+
+// apply use korte hole array hishebe parameter pathate hoy
+normalPerson.chargeBill.apply(heroPerson, [3000, 300, 30]);
+normalPerson.chargeBill.apply(heroPerson,[4000, 400, 40])
+console.log(heroPerson.salary);
