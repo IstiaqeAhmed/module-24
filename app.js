67e1d2c815d9ace88b5a54c0ffa8e54b5824291d
Istiaqe Ahmed Khan <istiaqeahmed.cse@gmail.com>
2021-02-07 15:57:04 +0600

    25_5-3 Create a lot of countries with style and forEach

--- app.js
+++ app.js
@@ -5,22 +5,45 @@ fetch('https://restcountries.eu/rest/v2/all')
 
 const displayCountries = countries => {
     const countriesDiv = document.getElementById('countries');
-    for (let i = 0; i < countries.length; i++) {
-        const country = countries[i];
-
-        const countryDiv = document.createElement('div');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(p);
+    countries.forEach(country => {
+       
+         const countryDiv = document.createElement('div');
+         countryDiv.className = 'country'
+         const countryInfo = `
+             <h3 class = "country-name">${country.name}</h3>
+             <p>${country.capital}</p>
         
-        countriesDiv.appendChild(countryDiv)
+        `;
+         countryDiv.innerHTML = countryInfo;
+         countriesDiv.appendChild(countryDiv);
+    });
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //      const countryDiv = document.createElement('div');
+    //      countryDiv.className = 'country'
+      
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+         // countryDiv.appendChild(h3);
+        
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // countryDiv.appendChild(p);
+
+
+        // const countryInfo = `
+        //      <h3 class = "country-name">${country.name}</h3>
+        //      <p>${country.capital}</p>
+        
+        // `
+        // countryDiv.innerHTML = countryInfo;
+        
+        // countriesDiv.appendChild(countryDiv)
 
 
         // li.innerText = country.name;
         // ul.appendChild(li);
         //console.log(country.name);
-    }
+    //}
+
 }
